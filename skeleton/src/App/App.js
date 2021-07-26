7f7fbbc4f587b915da1b79c476252e2d60dd9f73
LeeHanSeong <jkhk7788@gmail.com>
2021-07-26 23:41:53 +0900

    updaty

--- skeleton/src/App/App.js
+++ skeleton/src/App/App.js
@@ -2,103 +2,13 @@ import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
 import MainPanel from '../views/MainPanel';
 import Detail from '../views/Detail';
 import { useState } from 'react';
+import {getEventList, makeDummy} from '../Modules/eventModule'
 
-const testevents=[
-	{
-        title: 'All Day Event',
-        start: '2021-07-01'
-      },
-	  {
-        title: 'Long Event',
-        start: '2021-07-01',
-        end: '2021-07-10',
-        color: 'purple' // override!
-      },
-	  {
-        title: 'Long Event',
-        start: '2021-07-01',
-        end: '2021-07-10',
-        color: 'purple' // override!
-      },
-      {
-        title: 'Long Event',
-        start: '2021-07-07',
-        end: '2021-07-10',
-        color: 'purple' // override!
-      },
-	  {
-        title: 'Long Event2',
-        start: '2021-07-07',
-        end: '2021-07-10',
-        color: 'purple' // override!
-      },
-      {
-        groupId: '999',
-        title: 'Repeating Event',
-        start: '2021-07-09T16:00:00'
-      },
-      {
-        groupId: '999',
-        title: 'Repeating Event',
-        start: '2021-07-16T16:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2021-07-11',
-        end: '2021-07-13',
-        color: 'purple' // override!
-      },
-	  {
-        title: 'Conference2',
-        start: '2021-07-11',
-        end: '2021-07-13',
-        color: 'purple' // override!
-      },
-      {
-        title: 'Meeting',
-        start: '2021-07-12T10:30:00',
-        end: '2021-07-13',
-      },
-      {
-        title: 'Lunch',
-        start: '2021-07-12T12:00:00',
-		    end: '2021-07-13',
-      },
-      {
-        title: 'Lunch',
-        start: '2021-07-14T12:00:00',
-		    end: '2021-07-22',
-      },
-      {
-        title: 'Lunch',
-        start: '2021-07-14T12:00:00',
-		    end: '2021-07-22',
-      },
-      {
-        title: 'Meeting',
-        start: '2021-07-12T14:30:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2021-07-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2021-07-28',
-        end: '2021-08-28'
-      },
-	  {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2021-07-28',
-        end: '2021-08-28'
-      }
-]
+makeDummy();
 
 function App(props){
 	const [devices, setDevices] = useState([]);
-	const [events, setEvents] = useState(testevents);
+	const [events, setEvents] = useState(getEventList());
 	const pageList=['calendar','detail','setting'];
 	const [page, setPage] = useState({
 		path:['calendar'],
---
+++ skeleton/src/Modules/eventModule.js
@@ -0,0 +1,100 @@
+let events=[]
+
+export function getEventList(){
+    return events;
+}
+
+export function makeDummy(){
+    events = [
+        {
+            title: 'All Day Event',
+            start: '2021-07-01'
+          },
+          {
+            title: 'Long Event',
+            start: '2021-07-01',
+            end: '2021-07-10',
+            color: 'purple' // override!
+          },
+          {
+            title: 'Long Event',
+            start: '2021-07-01',
+            end: '2021-07-10',
+            color: 'purple' // override!
+          },
+          {
+            title: 'Long Event',
+            start: '2021-07-07',
+            end: '2021-07-10',
+            color: 'purple' // override!
+          },
+          {
+            title: 'Long Event2',
+            start: '2021-07-07',
+            end: '2021-07-10',
+            color: 'purple' // override!
+          },
+          {
+            groupId: '999',
+            title: 'Repeating Event',
+            start: '2021-07-09T16:00:00'
+          },
+          {
+            groupId: '999',
+            title: 'Repeating Event',
+            start: '2021-07-16T16:00:00'
+          },
+          {
+            title: 'Conference',
+            start: '2021-07-11',
+            end: '2021-07-13',
+            color: 'purple' // override!
+          },
+          {
+            title: 'Conference2',
+            start: '2021-07-11',
+            end: '2021-07-13',
+            color: 'purple' // override!
+          },
+          {
+            title: 'Meeting',
+            start: '2021-07-12T10:30:00',
+            end: '2021-07-13',
+          },
+          {
+            title: 'Lunch',
+            start: '2021-07-12T12:00:00',
+                end: '2021-07-13',
+          },
+          {
+            title: 'Lunch',
+            start: '2021-07-14T12:00:00',
+                end: '2021-07-22',
+          },
+          {
+            title: 'Lunch',
+            start: '2021-07-14T12:00:00',
+                end: '2021-07-22',
+          },
+          {
+            title: 'Meeting',
+            start: '2021-07-12T14:30:00'
+          },
+          {
+            title: 'Birthday Party',
+            start: '2021-07-13T07:00:00'
+          },
+          {
+            title: 'Click for Google',
+            url: 'http://google.com/',
+            start: '2021-07-28',
+            end: '2021-08-28'
+          },
+          {
+            title: 'Click for Google',
+            url: 'http://google.com/',
+            start: '2021-07-28',
+            end: '2021-08-28'
+          }
+    ]
+}
