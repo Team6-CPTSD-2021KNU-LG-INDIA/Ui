153166005042b95f8a7d559e0416474a7c7f9745
LeeHanSeong <jkhk7788@gmail.com>
2021-07-27 23:30:51 +0900

    update

--- skeleton/src/Modules/webosModule.js
+++ skeleton/src/Modules/webosModule.js
@@ -1,36 +1,10 @@
-var bridge;
-const servicename = ""
+
+let bridge = new WebOSServiceBridge();
+const config = require('./config.json');
+const servicename = config.serviceName;
 
 export function callAPI(method, params, callback){
-    const API="luna://com.domain.txtapp.txtservice/"+method;
-
+    const API=`luna://${servicename}/${method}`;
     bridge.onservicecallback = callback;
     bridge.call(API, params);
-}
-
-function setPathClicked(){
-    callAPI('setpath',`{
-        "path": "${document.getElementById("path").value}"
-    }`,(msg)=>{});
-    callAPI('ls',`{
-    }`,(msg)=>{
-        var arg = JSON.parse(msg);
-        let list = document.getElementById("list");
-        if (arg.returnValue) {
-            arg.Response.forEach(file=>{
-                let item = document.createElement("li");
-                item.innerHTML=file;
-                
-                item.addEventListener("click",(event)=>{
-                    callAPI('read',`{
-                        "name":"${event.target.innerHTML}", "encode":"utf8"
-                    }`,(msg)=>{
-                        var arg = JSON.parse(msg);
-                        document.getElementById("textarea").innerHTML = arg.Response;
-                    });
-                });
-                list.appendChild(item);
-            });
-        }
-    });
 }
