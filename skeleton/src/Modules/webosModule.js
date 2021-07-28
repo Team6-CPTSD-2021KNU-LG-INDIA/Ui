e9ff1f44d9efc83b4e75b655304618e9b822ec93
LeeHanSeong <jkhk7788@gmail.com>
2021-07-29 03:38:51 +0900

    update

--- skeleton/src/Modules/webosModule.js
+++ skeleton/src/Modules/webosModule.js
@@ -1,10 +1,17 @@
+import LS2Request from '@enact/webos/LS2Request';
 
-let bridge = new WebOSServiceBridge();
 const config = require('./config.json');
 const servicename = config.serviceName;
 
 export function callAPI(method, params, callback){
-    const API=`luna://${servicename}/${method}`;
-    bridge.onservicecallback = callback;
-    bridge.call(API, params);
+    const API=`luna://${servicename}/${method}/`;
+    return new LS2Request().send({
+		service: API,
+		method: 'callAPI',
+		parameters: params,
+		onSuccess: (res) => {
+			callback(res);
+		},
+		onFailure: (res) => console.error(res),
+	});
 }
