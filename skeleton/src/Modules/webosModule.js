2ed8820bda304e600f2655effc5527c57f73af97
LeeHanSeong <jkhk7788@gmail.com>
2021-07-30 02:07:32 +0900

    update

--- skeleton/src/Modules/webosModule.js
+++ skeleton/src/Modules/webosModule.js
@@ -6,17 +6,17 @@ const LS2 = new LS2Request();
 
 export function callAPI(method, params, callback){
     const API=`luna://${servicename}/`;
-	// try{
-	// 	return LS2.send({
-	// 		service: API,
-	// 		method: method,
-	// 		parameters: params,
-	// 		onSuccess: (res) => {
-	// 			callback(res);
-	// 		},
-	// 		onFailure: (res) => console.error(res),
-	// 	});
-	// }catch(e){
-	// 	return e.toString();
-	// }
+	try{
+		return LS2.send({
+			service: API,
+			method: method,
+			parameters: params,
+			onSuccess: (res) => {
+				callback(res);
+			},
+			onFailure: (res) => console.error(res),
+		});
+	}catch(e){
+		return e.toString();
+	}
 }
