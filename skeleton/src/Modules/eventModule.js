a526eac71a7d5ac5245c4099e3d3ec7620504ae3
LeeHanSeong <jkhk7788@gmail.com>
2021-08-01 14:53:03 +0900

    update

--- skeleton/src/Modules/eventModule.js
+++ skeleton/src/Modules/eventModule.js
@@ -16,105 +16,28 @@ export function loadEventList(setEvents){
   });
 }
 
+export function addEvent(event){
+  callAPI('addEvent',event,
+  (msg)=>{
+  },(msg)=>{
+  });
+}
+
 export function getEventList(){
   return events;
 }
 
-export function makeValidEvents(srcList,starttime, endtime){
+export function makeValidEvents(starttime, endtime){
   return events;
 }
 
-export function makeDummy(){
-    events = [
-        {
-            title: 'All Day Event',
-            start: '2021-07-01'
-          },
-          {
-            title: 'Long Event',
-            start: '2021-07-01',
-            end: '2021-07-10',
-            color: 'purple' // override!
-          },
-          {
-            title: 'Long Event',
-            start: '2021-07-01',
-            end: '2021-07-10',
-            color: 'purple' // override!
-          },
-          {
-            title: 'Long Event',
-            start: '2021-07-07',
-            end: '2021-07-10',
-            color: 'purple' // override!
-          },
-          {
-            title: 'Long Event2',
-            start: '2021-07-07',
-            end: '2021-07-10',
-            color: 'purple' // override!
-          },
-          {
-            groupId: '999',
-            title: 'Repeating Event',
-            start: '2021-07-09T16:00:00'
-          },
-          {
-            groupId: '999',
-            title: 'Repeating Event',
-            start: '2021-07-16T16:00:00'
-          },
-          {
-            title: 'Conference',
-            start: '2021-07-11',
-            end: '2021-07-13',
-            color: 'purple' // override!
-          },
-          {
-            title: 'Conference2',
-            start: '2021-07-11',
-            end: '2021-07-13',
-            color: 'purple' // override!
-          },
-          {
-            title: 'Meeting',
-            start: '2021-07-12T10:30:00',
-            end: '2021-07-13',
-          },
-          {
-            title: 'Lunch',
-            start: '2021-07-12T12:00:00',
-                end: '2021-07-13',
-          },
-          {
-            title: 'Lunch',
-            start: '2021-07-14T12:00:00',
-                end: '2021-07-22',
-          },
-          {
-            title: 'Lunch',
-            start: '2021-07-14T12:00:00',
-                end: '2021-07-22',
-          },
-          {
-            title: 'Meeting',
-            start: '2021-07-12T14:30:00'
-          },
-          {
-            title: 'Birthday Party',
-            start: '2021-07-13T07:00:00'
-          },
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/',
-            start: '2021-07-28',
-            end: '2021-08-28'
-          },
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/',
-            start: '2021-07-28',
-            end: '2021-08-28'
-          }
-    ]
-}
+export class eventInfo{
+  constructor(device_id, action_id, title, contents, start, end){
+      this.device_id = device_id;
+      this.action_id = action_id;
+      this.title = title;
+      this.contents = contents;
+      this.start = start;
+      this.end = end;
+  }
+};
