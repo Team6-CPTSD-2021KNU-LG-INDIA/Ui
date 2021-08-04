d0449f1c521acb6fa7a29b5429b7d6880643dce9
LeeHanSeong <jkhk7788@gmail.com>
2021-08-04 16:55:16 +0900

    update

--- skeleton/src/Modules/iotModule.js
+++ skeleton/src/Modules/iotModule.js
@@ -1,20 +1,24 @@
 let Devices = [];
 
 class DeviceInfo{
-    constructor(name,id,connection){
+    constructor(name,id,apilist,connection){
       this.name = name;
       this.id = id;
+      this.apilist = apilist;
       this.connection = connection;
     }
 }
 
 export function makeDummy(){
-    ['1','2','3','4','5','6','7','8','9','10'].forEach((item)=>{
-        Devices.push(new DeviceInfo(item,'id',true));
-      });
-      
+  ['1','2','3','4','5','6','7','8','9','10'].forEach((item)=>{
+      Devices.push(new DeviceInfo(item,'id',[],true));
+  });
 }
 
 export function getDevices(){
     return Devices;
+}
+
+export function loadDevices(){
+  
 }
